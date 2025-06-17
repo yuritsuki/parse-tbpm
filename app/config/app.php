6d89017f3548f780b2ded9dad685d4d72b61a706
Yuritsuki <samatkamiev@gmail.com>
2025-06-17 11:19:35 +0500

    laravel excel added

--- app/config/app.php
+++ app/config/app.php
@@ -56,6 +56,7 @@ return [
     'providers' => [
         App\Providers\AppServiceProvider::class,
         Intonate\TinkerZero\TinkerZeroServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
     ],
 
 ];
